7aba43d1b5c0802e5276b8aaed889bd5a8c428ce
Houssem Dellai <houssem.dellai@live.com>
2017-04-06 17:49:23 +0200

    Renamed client app.

---
+++ XamarinApp/XamarinApp/Models/AuthResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace XamarinApp.Models
+{
+    public class AuthResponse
+    {
+        /// <summary>   
+        /// Tells if the API call was processed successfully.
+        /// </summary>
+        public bool IsSuccess { get; set; }
+
+        /// <summary>
+        /// Result received from the API.
+        /// Could be JSON or String.
+        /// </summary>
+        public string Result { get; set; }      
+    }
+}
